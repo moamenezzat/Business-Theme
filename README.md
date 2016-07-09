0ddcb69fc138e34571d205df2e04f3327e4c7aed
Mo'men EzzatMo'men EzzatMo'men Ezzat <smarty21000@gmail.com>
2016-07-09 03:13:02 +0200

    Added header and services section

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 Design Courtesy of akhilwebfolio.
 PSD Link: http://365psd.com/psd/free-business-theme-psd-61762
+
+Live Demo: https://moamenezzat.github.io/Business-Theme/
---
+++ functions.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+    var imgs = $(".slider img"),
+        indicators = $(".indicator span"),
+        imgsNum = imgs.length,
+        IndicatorsNum = indicators.length;
+
+    // Menu Expansion
+//     if (matchMedia) {
+//     var mq = window.matchMedia("(max-width: 740px)");
+//     mq.addListener(console.log("changed"));
+//     WidthChange(mq);
+// }
+//     function WidthChange(mq){
+//       var menuHeight = -($(".menu").height())-10;
+//       if (mq.matches) {
+//         console.log(menuHeight);
+//     $(".services").css("transform", "translateY("+(menuHeight)+"px)");
+//     $(".menu-expand").on("click", function(){
+//       $(".services").toggleClass("down");
+//     });}else{
+//       $(".services").css("transform", "translateY("+0+"px)");
+//     }};
+$(".menu-expand").on("click", function(){
+      $(".menu").toggleClass("open");
+    });
+    // Time Loop Function
+    setInterval(function() {
+        var curImg = imgs.index($(".in"));
+        if (curImg < imgsNum - 1) {
+            imgs.eq(curImg).removeClass("in").addClass("out").next().removeClass("out").addClass("in");
+            indicators.eq(curImg).removeClass("active").next().addClass("active");
+        } else {
+            imgs.eq(curImg).removeClass("in").addClass("out");
+            imgs.eq(0).removeClass("out").addClass("in");
+            indicators.eq(curImg).removeClass("active")
+            indicators.eq(0).addClass("active");
+        };
+    }, 5000);
+
+    // Indicators Clicking Function
+    indicators.on("click", function() {
+        var $this = $(this),
+            $position = indicators.index($this);
+        indicators.removeClass("active").eq($position).addClass("active");
+        imgs.removeClass("in out").eq($position).addClass("in");
+        imgs.not(imgs.eq($position)).addClass("out");
+    });
+})
